430c11aadbe312f0c95398fa46ebe670194f3e63
Prasannakumar-1234 <prasanna1234mpk@gmail.com>
2026-06-03 10:09:07 +0530

    Add files via upload

---
+++ 1 test full.py
@@ -0,0 +1,52 @@
+1.)
+
+n = input()
+print(n)
+
+2.)
+
+print("MECON")
+print("stadium")
+
+3.)
+
+name1 = input()
+score1 = int(input())
+over1 = int(input())
+name2 = input()
+score2 = int(input())
+over2 = int(input())
+print("match details")
+print("team 1")
+print("name:",name1)
+print("score:",score1)
+print("over:",over1)
+print("team 2")
+print("name:",name2)
+print("score:",score2)
+print("over:",over2)
+
+4.)
+
+name = input()
+game = input()
+
+print("My name is", name, "and I love to play", game)
+
+5.)
+
+Age = int(input())
+print("Age of Dhoni is",Age)
+
+6.)
+
+ch = input()
+print(ch)
+
+7.)
+
+speed = float(input())
+
+
+print(f"{speed:.2f}")
+
